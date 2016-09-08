1710d97a0538210bb70756b0b842b826a335c390
Ji Woong <park.jiwoong@wustl.edu>
2016-09-08 01:05:02 -0500

    Studio9 Done

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ terrible_movies = [
     "Nine Lives"
 ]
 
-
 class Movie(db.Model):
     title = db.StringProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
@@ -147,7 +146,8 @@ class MovieRatings(Handler):
         if movie and rating:
             # TODO 3
             # update the movie's rating property and save it to the database
-
+            movie.rating = rating
+            movie.put()
 
             # render confirmation
             t = jinja_env.get_template("rating-confirmation.html")
